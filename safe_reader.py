b78d164d71c918c60919a8703f78217dd3a5c7e3
guado1983-star <guado1983@gmail.com>
2026-06-02 21:53:08 -0400

    Delete safe_reader.py

--- safe_reader.py
+++
@@ -1,8 +0,0 @@
-while True:
-    filename = input("Enter the filename to open: ").strip()
-    try:
-        with open(filename, "r") as file:
-            print(file.read())
-        break
-    except FileNotFoundError:
-        print("File not found, try again")
