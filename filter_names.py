831fb6ea27311c8941172c93029209889be4e71d
guado1983-star <guado1983@gmail.com>
2026-06-02 21:56:04 -0400

    Delete filter_names.py

--- filter_names.py
+++
@@ -1,8 +0,0 @@
-with open("names.txt", "r") as read_file:
-    with open("short_names.txt", "w") as write_file:
-        for line in read_file:
-            name = line.strip()
-            if len(name) <= 4 and name != "":
-                write_file.write(name + "\n")
-
-print("Done! Check your sidebar for short_names.txt")
