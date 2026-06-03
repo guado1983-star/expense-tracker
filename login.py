b5b7917cdd92cf181eaf0c86752776a4bd9f5475
guado1983-star <guado1983@gmail.com>
2026-06-02 21:58:56 -0400

    Delete login.py

--- login.py
+++
@@ -1,6 +0,0 @@
-username = input("Enter username:")
-password = input("Enter password:")
-if username == "admin" and password == "seceret123":
-    print("Welcome admin")
-else:
-    print("Access denied")
